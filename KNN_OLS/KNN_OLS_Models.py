cd841cd2b6bd08ea8afe2c3b76b5b105d565f66d
KatherineLRobles <katherinerobles925@gmail.com>
2026-05-11 22:34:42 -0400

    Update KNN_OLS_Models.py

--- KNN_OLS/KNN_OLS_Models.py
+++ KNN_OLS/KNN_OLS_Models.py
@@ -30,7 +30,7 @@ def _evaluate(name, model, X_train, y_train, X_test, y_test):
     return metrics
 
 
-def run_KNN_OLS_models(X_train, X_test, y_train, y_test, tscv, feature_names):
+def run_KNN_OLS_models(X_train, X_test, y_train, y_test):
     
     # Trains OLS and KNN models, returns dict of test-set metrics for both models
     
